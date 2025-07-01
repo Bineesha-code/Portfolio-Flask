f1525781f1a31dd7935beb4112c6269eb8e26e12
Bineesha-code <bineeshaharilal@gmail.com>
2025-07-01 16:10:00 +0000

    Initial commit - Flask Portfolio Website

---
+++ app.py
@@ -0,0 +1,22 @@
+# Task-5
+# Portfolio Website with Flask
+# Author: Bineesha K P
+# Date: 26-06-2025
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    response = f"Thank you, {name}! Your message has been received."
+    return render_template('index.html', message=response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
